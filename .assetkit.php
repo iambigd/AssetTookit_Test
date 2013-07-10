2518a26948e23af01011cc0a0130315cc972ef59
bigd <ken@iangel.tw>
2013-07-10 17:03:31 +0800

    add .assetkit.php

---
+++ .assetkit.php
@@ -0,0 +1,38 @@
+<?php return array (
+  'baseDir' => 'public/assets',
+  'baseUrl' => '/assets',
+  'dirs' => 
+  array (
+  ),
+  'cache_dir' => 'cache',
+  'namespace' => '/var/www/static',
+  'pages' => 
+  array (
+  ),
+  'assets' => 
+  array (
+    'jquery' => 
+    array (
+      'manifest' => 'assets/jquery/manifest.php',
+      'source_dir' => 'assets/jquery',
+      'name' => 'jquery',
+    ),
+    'iangel' => 
+    array (
+      'manifest' => 'assets/iangel/manifest.php',
+      'source_dir' => 'assets/iangel',
+      'name' => 'iangel',
+    ),
+  ),
+  'target' => 
+  array (
+    'bigd-test' => 
+    array (
+      0 => 'jquery',
+    ),
+    'bigd-asset' => 
+    array (
+      0 => 'iangel',
+    ),
+  ),
+);
