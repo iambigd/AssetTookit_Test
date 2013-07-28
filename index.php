f536338de20cc9902952d810d34622a27b50800b
bigd <ken@iangel.tw>
2013-07-28 17:17:53 +0800

    update

--- index.php
+++ index.php
@@ -6,26 +6,33 @@ require "vendor/autoload.php";
 
 $baseUrl = dirname($_SERVER['SCRIPT_NAME']);
 
-//your app folder
-$APPLICATION_ROOT = "/var/www/static";
+//應用程式的目錄
+define('ROOT', dirname(__DIR__) );
+// echo ROOT;
 
 //please use correct .assetkit.php file
+//使用絕對路徑參照
+// In production mode, the asset compiler squashes the loaded asset files to the minified files.
+// In development mode, the asset compiler simply render the include paths.
 $config = new AssetToolkit\AssetConfig( '.assetkit.php', array( 
-    'root' => $APPLICATION_ROOT 
-    ,'environment' => AssetToolkit\AssetConfig::PRODUCTION
-    // ,'environment' => AssetToolkit\AssetConfig::DEVELOPMENT
+    // 'root' => ROOT, 
+    'environment' => AssetToolkit\AssetConfig::PRODUCTION
+    // 'environment' => AssetToolkit\AssetConfig::DEVELOPMENT
 ));
 
 // initialize an asset loader
 $loader = new AssetToolkit\AssetLoader( $config );
 $assets = array();
-// $assets[] = $loader->load( 'jquery' );
+
+//指定本頁會用到的assets
+$assets[] = $loader->load( 'jquery' );
 $assets[] = $loader->load( 'iangel' );
 
-
+//產生asset的render
 $render = new AssetToolkit\AssetRender($config,$loader);
 
 // Use AssetRender to compile and render the HTML tag
+// 動態產生,如果指定的asset有改變,會自動產生新版本,但先前的版本不會自動砍掉
 $compiler = $render->getCompiler();
 $compiler->enableProductionFstatCheck();
 // $compiler->defaultJsCompressor = 'uglifyjs';
@@ -35,7 +42,7 @@ $render->force();
 <html>
 <head>
 <?php
-$render->renderAssets($assets,'bigd-asset');
+$render->renderAssets($assets);
 ?>
 <style>
 body { font-size: 12px; }
